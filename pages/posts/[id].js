41cef985c5b3c3e5c86d496d6dee7b8bd240581d
ryosuketter <r.shimizu.yale@gmail.com>
2022-08-14 14:56:58 +0900

    feat Head meta description

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -27,6 +27,7 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
+        <meta name="description" content={postData.title} />
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
